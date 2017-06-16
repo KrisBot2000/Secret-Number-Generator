4d4a40bc010e30595acae6ba19e2c42521da9db1
Kristin Treder <stuffs@Kristins-MacBook.local>
2017-06-15 14:13:05 -1000

    final draft

--- secret_number.js
+++ secret_number.js
@@ -3,38 +3,10 @@
 module.exports = function() {
 
 
-var sameRandomNumber = 0;
-var genRandomNumber = Math.random();
+
+const SAME_NUM = Math.floor(Math.random()*1000001);
 
   return function(){
-  sameRandomNumber = genRandomNumber*1000001;
-  return sameRandomNumber;
-  }
-
-
-
-
-
-
-// var money = 55.50;
-// var coffeeCost = 8.50;
-
-// var drinkCoffee = (function(){
-//   var cupsDrunk = 0;
-//   return function(){
-//     if( money >= coffeeCost ){
-//       money -= coffeeCost;
-//       cupsDrunk++;
-//     }
-//     return cupsDrunk; // return the value
-//   };
-// })();
-
-// var count;
-// count = drinkCoffee();  // 1
-// count = drinkCoffee();  // 2
-// count = drinkCoffee();  // 3
-// console.log( count );  // 3
-// console.log( money );  // 30
-
+    return SAME_NUM;
+  };
 };
